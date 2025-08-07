08c9f45c4dbdfd154d129dcfdf87b14084834cb0
marimunari <marianamunari4@gmail.com>
2025-08-07 19:47:31 -0300

    chore: add configuration and package files
    Includes Babel, EditorConfig, Jest, PostCSS, and package management files

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(axios)/)'
+  ],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+}
